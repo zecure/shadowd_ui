beed62cad7a02e3b5e1b15abdf4b7a18931c94e9
zit-hb <hb@zecure.org>
2015-06-24 01:00:06 +0200

    Whups, forgot to delete a joke between friends in the beginning of the project ;)

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,10 +7,6 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-if ($_SERVER['HTTP_HOST'] === '2g6r3z4zuplw4flu.onion') {
-    exit('Fuck off, mate');
-}
-
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
